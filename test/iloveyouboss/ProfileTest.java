e1d71024f20aa543f2b2092907b445c6af0d25a7
Yun Wang <yun.wang@rea-group.com>
2016-01-19 18:17:18 +0800

    fix pom issues and add target to ignore

---
+++ test/iloveyouboss/ProfileTest.java
@@ -0,0 +1,16 @@
+package iloveyouboss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yunwang on 1/14/16.
+ */
+public class ProfileTest {
+
+    @Test
+    public void testMatches() throws Exception {
+
+    }
+}
