3f1761a3ead2b7559c24f75b371abc839c0cef73
Yun Wang <yun.wang@rea-group.com>
2016-01-19 18:53:03 +0800

    refactor test.

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -8,15 +8,27 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfileTest {
+    private static Question question;
+    private static Criterion criterion;
+
+    private Criteria criteria;
+    private Profile profile;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question = new BooleanQuestion(1, "Are you a girl?");
+        criterion = new Criterion(new Answer(question, 0), Weight.Important);
+    }
+
+    @Before
+    public void before() {
+        criteria = new Criteria();
+        profile = new Profile("Q1");
+    }
 
     @Test
     public void should_return_1000_score_and_true_matches_when_answer_one_important_question_correctly() {
-        Question question = new BooleanQuestion(1, "Are you a girl?");
-
-        Criterion criterion = new Criterion(new Answer(question, 0), Weight.Important);
-        Criteria criteria = new Criteria();
         criteria.add(criterion);
-        Profile profile = new Profile("Q1");
         profile.add(new Answer(question, 0));
 
         assertThat(profile.matches(criteria), is(true));
@@ -25,14 +37,8 @@ public class ProfileTest {
 
     @Test
     public void should_return_0_score_and_false_matches_when_answer_one_important_question_wrongly() {
-        Question question = new BooleanQuestion(1, "Are you a girl?");
-
-        Profile profile = new Profile("Q1");
-        profile.add(new Answer(question, 0));
-
-        Criterion criterion = new Criterion(new Answer(question, 1), Weight.Important);
-        Criteria criteria = new Criteria();
         criteria.add(criterion);
+        profile.add(new Answer(question, 1));
 
         assertThat(profile.matches(criteria), is(false));
         assertThat(profile.score(), is(0));
